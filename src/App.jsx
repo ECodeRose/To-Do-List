eeb0a0afc59c70764f85d258a5fd25632ac83baf
Ethan Rose <ethanrose.er@googlemail.com>
2024-01-28 22:51:12 +0000

    Cleaned up template

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,3 @@
-import { useState } from 'react'
-import './App.css'
-
-const App= () => {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  )
+export default function App() {
+  return "Hi"
 }
-
-export default App
